9b23b0fdbe78d716ab789cf6230f597678df537a
SebaOgas <saoga07.sao@gmail.com>
2025-07-10 19:26:33 -0300

    Popup creado

---
+++ src/lib/components/Popup.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let title : string | undefined = "";
+
+    export let visible : boolean = false;
+
+    export let fitW : boolean = false;
+    export let fitH : boolean = false;
+</script>
+
+{#if visible}
+    <div id="bg" class="fixed top-0 left-0 bottom-0 right-0 bg-dark flex items-center justify-center p-4">
+        <div id="popup" class="bg-white rounded-lg p-4 w-{fitW ? "fit" : "full"} h-{fitH ? "fit" : "full"}">
+            <h1 class="text-m">{title}</h1>
+            <slot/>
+        </div>
+    </div>
+{/if}
+
+<style>
+    #bg {
+        background-color: #1D4E89b0;
+    }
+
+    #popup {
+        opacity: 1;
+    }
+</style>
--- src/routes/Link3/+page.svelte
+++ src/routes/Link3/+page.svelte
@@ -1,1 +1,8 @@
+<script>
+	import Popup from "$lib/components/Popup.svelte";
+
+</script>
 <p>Contenido de la página 3</p>
+
+
+<Popup title="Título" visible={true}>Hola</Popup>
