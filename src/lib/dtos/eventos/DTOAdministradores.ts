b6f6d9967137e51f3ba8f2634c0f9960d775e70c
SebaOgas <saoga07.sao@gmail.com>
2025-09-07 09:11:13 -0300

    Gestionar admins de evento listo

---
+++ src/lib/dtos/eventos/DTOAdministradores.ts
@@ -0,0 +1,18 @@
+export default interface DTOAdministradores {
+    esOrganizador: boolean,
+
+    nombreEvento: string,
+
+    administradores: {
+        nombre: string,
+        apellido: string,
+        username: string,
+
+        vigente: boolean,
+
+        historico: {
+            fechaDesde: Date,
+            fechaHasta: Date | null
+        }[]
+    }[]
+}
