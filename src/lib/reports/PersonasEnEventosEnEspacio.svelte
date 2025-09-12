4d28efd8d33000723323b5f526ee4fd4ef5bb957
SebaOgas <saoga07.sao@gmail.com>
2025-09-12 12:08:38 -0300

    Página de Reportes creada

---
+++ src/lib/reports/PersonasEnEventosEnEspacio.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    
+</script>
+
+
+<div class="flex flex-col md:flex-row gap-2 justify-between">
+    <div class="flex-row gap-2">
+        <span>Espacio: </span>
+    </div>
+</div>
